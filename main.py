355a6d6d59dbee9ee6cc8b8c1b79532cedad52c3
d3-worgan <daniel2.worgan@live.uwe.ac.uk>
2020-11-13 12:10:45 +0000

    comments and clean

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-import json
 import sys
-import os
 
 from PyQt5.QtWidgets import QStatusBar, QFileDialog, QErrorMessage, QAction, QToolBar, QMessageBox, QInputDialog, QLineEdit, QWidget, QVBoxLayout, QApplication, QMainWindow, QTreeView, QCheckBox, QTreeWidget, QTreeWidgetItem, QPushButton
-from PyQt5.Qt import QStandardItemModel, QStandardItem
-from PyQt5.QtCore import Qt
-import diagnostic_significance
 
 import qtmodern.styles
 import qtmodern.windows
-import dialogs
-from MainWindow import MainWindow
+from main_window import MainWindow
 
 
 if __name__ == '__main__':
-    mod_window = False
+    mod_window = False  # Choose if we want styling
     if mod_window:
         app = QApplication(sys.argv)
         qtmodern.styles.light(app)

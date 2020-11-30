4e5f0874c68d32a2b8148e088c478211fe441e9b
d3-worgan <daniel2.worgan@live.uwe.ac.uk>
2020-11-30 13:01:06 +0000

    Swapped qt designer files. Now implemented patient info, recording info, background features, diagnostic significance. Began save/load functionality

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 import sys
-
-from PyQt5.QtWidgets import QStatusBar, QFileDialog, QErrorMessage, QAction, QToolBar, QMessageBox, QInputDialog, QLineEdit, QWidget, QVBoxLayout, QApplication, QMainWindow, QTreeView, QCheckBox, QTreeWidget, QTreeWidgetItem, QPushButton
-
-import qtmodern.styles
-import qtmodern.windows
+from PyQt5.QtWidgets import QApplication
 from main_window import MainWindow
 
 
 if __name__ == '__main__':
-    mod_window = False  # Choose if we want styling
-    if mod_window:
-        app = QApplication(sys.argv)
-        qtmodern.styles.light(app)
-        mw = qtmodern.windows.ModernWindow(MainWindow())
-        mw.show()
-        sys.exit(app.exec_())
-    else:
-        app = QApplication(sys.argv)
-        demo = MainWindow()
-        demo.show()
-        sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())

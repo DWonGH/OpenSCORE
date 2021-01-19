47732cc54052caf32ecef4a6627535bfb02edf19
d3-worgan <daniel2.worgan@live.uwe.ac.uk>
2021-01-19 13:55:21 +0000

    Organised according to TEETACSI

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ from PyQt5.QtWidgets import QApplication, QTabWidget
 
 from src.controllers.main_window_controller import MainWindowController
 
-app = QApplication(sys.argv)
-app.setApplicationName("OpenSCORE")
-app.setOrganizationName("OpenSCORE")
-app.setAttribute(Qt.AA_UseHighDpiPixmaps)
 
-main_window_controller = MainWindowController()
-main_window_controller.view.show()
+def main():
+    app = QApplication(sys.argv)
+    app.setApplicationName("OpenSCORE")
+    app.setOrganizationName("OpenSCORE")
+    app.setAttribute(Qt.AA_UseHighDpiPixmaps)
 
-sys.exit(app.exec_())
+    main_window_controller = MainWindowController()
+    main_window_controller.view.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
